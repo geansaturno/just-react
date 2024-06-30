12cdac1c4a125c992a2f34c5aef4ac0d9286cdeb
Gean Saturno <geangoncalves.gst@ciclic.com.br>
2024-06-30 16:53:13 -0300

    chore: first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyButton from "./components/MyButton";
+
+export default function App() {
+    return (
+        <div>
+            <p>Oi eu sou o app</p>
+
+            <MyButton/>
+        </div>
+    )
+}
